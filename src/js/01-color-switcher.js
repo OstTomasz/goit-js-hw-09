c2fab2b58047416234e1c4f9acea00c85a9488dc
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-03-13 19:11:06 +0100

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+let interval;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1);
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  startBtn.disabled = false;
+});
