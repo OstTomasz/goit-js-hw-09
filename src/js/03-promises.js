bc6719db9cab7a99dd05e99b1da84fbb83f89642
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-03-19 15:54:28 +0100

    start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+const form = document.querySelector('.form');
+const amount = document.querySelector("input[name='amount']");
+// const inputs = document.querySelectorAll('input');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // inputs.forEach(input => {
+  //   input.value = '';
+  // });
+  createPromise();
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
