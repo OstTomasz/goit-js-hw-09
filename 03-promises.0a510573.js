e4071d651aadd54185d129b16530134cf1227982
OstTomasz <114577269+OstTomasz@users.noreply.github.com>
2023-03-19 14:54:53 +0000

    Deploying to gh-pages from @ OstTomasz/goit-js-hw-09@bc6719db9cab7a99dd05e99b1da84fbb83f89642 🚀

---
+++ 03-promises.0a510573.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form");document.querySelector("input[name='amount']");e.addEventListener("submit",(e=>{e.preventDefault(),Math.random()}));
+//# sourceMappingURL=03-promises.0a510573.js.map
---
+++ 03-promises.3ffa0104.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".form");document.querySelector("input[name='amount']");e.addEventListener("submit",(function(e){e.preventDefault(),Math.random()}))}();
+//# sourceMappingURL=03-promises.3ffa0104.js.map
