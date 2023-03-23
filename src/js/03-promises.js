73db0b234e39fae067dc407bb3078575aee09188
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-03-23 20:03:46 +0100

    z3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,63 @@
+import Notiflix from 'notiflix';
+
 const form = document.querySelector('.form');
-const amount = document.querySelector("input[name='amount']");
-// const inputs = document.querySelectorAll('input');
+const button = document.querySelector('button');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  // inputs.forEach(input => {
-  //   input.value = '';
-  // });
-  createPromise();
+  button.disabled = true;
+  let amount = Number(document.querySelector(`input[name="amount"]`).value);
+  let delay = Number(document.querySelector(`input[name="delay"]`).value);
+  let step = Number(document.querySelector(`input[name="step"]`).value);
+  let i = 0;
+  let jump = 0;
+  setTimeout(() => {
+    createPromise(1, delay);
+    i++;
+    jump = delay + step;
+    const interval = setInterval(() => {
+      if (i === amount) {
+        clearInterval(interval);
+        button.disabled = false;
+      } else {
+        createPromise(i + 1, jump);
+        jump = jump + step;
+        i++;
+      }
+    }, step);
+  }, delay);
 });
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//
+//   })
+//   .catch(({ position, delay }) => {
+
+//   });
+
+// const inputs = document.querySelectorAll('input');
+// inputs.forEach(input => {
+//   input.value = '';
+// });
+
+// for (let i = 1; i <= amount; i++) {
+//   let time;
+//   if (i === 1) {
+//     time = delay;
+//   } else {
+//     time = step;
+//   }
+//   setTimeout(() => {
+//     createPromise();
+//   }, time);
+// }
